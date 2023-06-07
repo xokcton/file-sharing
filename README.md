5248c154c6c4ff8a8d482f5759e424d733b900f4
xokcton <qwertyman2023@gmail.com>
2023-06-07 16:09:03 +0300

    set up configs

---
+++ README.md
@@ -0,0 +1,9 @@
+# File Sharing APP
+
+To fire up the project follow this steps:
+
+1. `cd ./file-sharing`
+1. `mv .env.example .env`
+1. fill up the environment variables
+1. `npm ci`
+1. `npm run build && npm start`
---
+++ server.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+import express, { Express } from 'express';
+
+const app: Express = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+});
